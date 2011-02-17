d4907b05ce1bcdda350619c3fcd138bae72f2638
Josh Schonstal <josh@schonstal.org>
2011-02-16 21:10:03 -0500

    Added social media icons

--- wp-content/themes/atmosphere-2010/footer.php
+++ wp-content/themes/atmosphere-2010/footer.php
@@ -3,7 +3,13 @@
 
 	<div id="footer" role="contentinfo">
 		<div id="colophon">
-
+			<div align="center">
+				<a href="http://facebook.com/pewpewpewpewpewpewpewpewpew"><img src="http://incredibleape.com/social/Facebook.png"></a>
+				<a href="https://twitter.com/#!/IAOfficial"><img src="http://incredibleape.com/social/Twitter.png"></a>
+				<a href="http://incredibleape.com/feed"><img src="http://incredibleape.com/social/RSS.png"></a>
+				<a href="http://vimeo.com/incredibleape"><img src="http://incredibleape.com/social/Vimeo.png"></a>
+				<a href="http://www.youtube.com/incredibleapegames"><img src="http://incredibleape.com/social/YouTube.png"></a>
+			</div>
 <?php get_sidebar( 'footer' ); ?>
 
 
